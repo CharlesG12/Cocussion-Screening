d24c11de534b3cfd48a94c18b880053cee84f4b2
CharlesG12 <charles.cheng.guo@gmail.com>
2018-10-30 15:32:07 -0500

    Add files via upload
    add random generate number script

---
+++ Assets/Script/swapTextures.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swapTextures : MonoBehaviour {
+    public Texture[] textures;
+    public int currentTexture;
+    private int updated = 0;
+
+	// Use this for initialization
+	void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if( updated ==0  )
+        {
+            currentTexture = Random.Range(0, 10);
+            Renderer m_renderer = GetComponent<Renderer>();
+            m_renderer.material.mainTexture = textures[currentTexture];
+            updated = 1;
+        }
+    }
+}
