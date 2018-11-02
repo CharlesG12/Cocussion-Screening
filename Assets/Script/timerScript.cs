790a683fcf8718e6ba1f16c204a9044dad543101
Sudeep Maity <sudeep.nitg@gmail.com>
2018-11-01 20:25:26 -0500

    Update Contains : ray casting and selection, transition from menu screen to scene1.

---
+++ Assets/Script/timerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class timerScript : MonoBehaviour {
+
+    private string levelToLoad = "Scene2";
+    private float timer = 10f;
+    private Text timerSec;
+
+	// Use this for initialization
+	void Start () {
+        timerSec = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        timerSec.text = timer.ToString("f2");
+        if(timer <= 0)
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+	}
+}
