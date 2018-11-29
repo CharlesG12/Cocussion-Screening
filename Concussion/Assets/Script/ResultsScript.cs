9f1d7c3db14efd1ee17b7643d0ed57b97e443b29
Charles Guo <charles.cheng.guo@gmail.com>
2018-11-29 12:42:33 -0600

    newest code

---
+++ Concussion/Assets/Script/ResultsScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ResultsScript : MonoBehaviour{
+
+    public Text timeElapsed;
+    public Text IncorrectView;
+
+    void Start(){
+        Results.Init();
+        Results.SetScore("scene2", "time", "190");
+
+        timeElapsed.text = Results.GetScore("scene1", "time");
+        IncorrectView.text = Results.GetScore("scene2", "time");
+     }
+
+    void Update()
+    {
+      
+    }
+}
